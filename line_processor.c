d5ef6c751cb54665906ca267821b5dbba2558821
yevgeniychaplygin3 <59663599+yevgeniychaplygin3@users.noreply.github.com>
2021-11-08 12:47:17 -0800

    finished preliminary functions.
    Now create threads.

--- line_processor.c
+++ line_processor.c
@@ -6,35 +6,78 @@
 #define SIZE 1000
 #define NUM_LINES 50
 
-char *buffer_1[NUM_LINES];
-// char buffer_2[SIZE];
+char *buffer_1[NUM_LINES], *buffer_2[NUM_LINES], *buffer_3[NUM_LINES];
 // number of items in buffer 1
-int count_1 = 0;
+int count_1 = 0, count_2 = 0, count_3 = 0;
 // index of the producer/input thread is going to be
-int prod_idx_1 = 0;
+int prod_idx_1 = 0, prod_idx_2 = 0, prod_idx_3 = 0;
 // index of the consumer/output thread is going to be
-int con_idx_1 = 0;
+int con_idx_1 = 0, con_idx_2 = 0, con_idx_3 = 0;
 
-// void put_buff_2(){
-//     for(int i=0;i<strlen(buffer_1);i++){
-//         buffer_1[prod_idx_1] = line[i];
-//         prod_idx_1 +=1;
-//         count_1++;
-//     }
-// }
+char* get_buff_3(){
+    char* aStringInput = buffer_3[con_idx_3];
+    con_idx_3 = con_idx_3 + 1;
+    count_2--;
+    return aStringInput;
+}
+void output_thread(){
+    char* printString = get_buff_3();
+    printf("Final output string: %s\n", printString);
+}
+void put_buff_3(char* string3){
+    // add in mutex & condition variables
+    buffer_3[prod_idx_3] = string3;
+    prod_idx_3 = prod_idx_3 + 1;
+    count_3++;
+}
+char* get_buff_2(){
+    // add in mutex & condition variables
+    char* aStringInput = buffer_2[con_idx_2];
+    con_idx_2 = con_idx_2 + 1;
+    count_2--;
+    return aStringInput;
+}
+
+void plus_sign_thread(){
+    for(int i=0; i<2;i++){
+        char *spaceSeperatedString = get_buff_2();
+        int len = strlen(spaceSeperatedString);
+        char copyString[len];
+        strcpy(copyString, spaceSeperatedString);
+        for (int j =0; j<len; j++){
+            strcpy(copyString, spaceSeperatedString);
+            if(copyString[j] == '+' && copyString[j+1] == '+' && j+1<len){
+                copyString[j] = '%';
+                copyString[j+1] = 'c';
+                sprintf(spaceSeperatedString, copyString, '^');
+            }
+        }
+        put_buff_3(spaceSeperatedString);
+    }
+}
+void put_buff_2(char *line_seperator_string){
+    // add in mutex & condition variables
+    buffer_2[prod_idx_2] = line_seperator_string;
+    prod_idx_2 +=1;
+    count_2++;
+}
 char* get_buff_1(){
+    // add in mutex & condition variables
     char* aStringInput = buffer_1[con_idx_1];
+    con_idx_1 = con_idx_1 + 1;
     count_1--;
     return aStringInput;
 }
 void line_seperator(){
-    char* aString = get_buff_1();
-    for(int i=0;i<       ;i++){
-        if(buffer_1[i] == '\n'){
-            buffer_1[i] = ' ';
+    for(int i=0;i<2;i++){
+        char* aString = get_buff_1();
+        int newLineIndex = strlen(aString)-1;
+        if(aString[newLineIndex] == '\n'){
+            aString[newLineIndex] = ' ';
+            printf("new line\n");
+            put_buff_2(aString);
         }
     }
-    put_buff_2();
 }
 void put_buff_1(char *line){
     // add in mutex & condition variables
@@ -42,7 +85,6 @@ void put_buff_1(char *line){
     prod_idx_1 +=1;
     count_1++;
 }
-
 char* get_user_input(){
     char *user_input = calloc(SIZE, sizeof(char));
     printf("Enter an input (no longer than 1000 characters): ");
@@ -50,16 +92,16 @@ char* get_user_input(){
     return user_input;
 }
 void get_input(){
-    for (int i=0; i<NUM_LINES; i++){
+    for (int i=0; i<2; i++){
         char *line_input = get_user_input();
         // fgets(line_input, SIZE, stdin);
         put_buff_1(line_input);
     }
 }
 int main(int argc, char *argv[]){
-
     get_input();
     line_seperator();
+    plus_sign_thread();
+    output_thread();
     return 0;
 }
-
