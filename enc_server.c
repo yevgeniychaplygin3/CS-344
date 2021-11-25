542da06e28d4e822fa4ab3ed18bae780d434ad8a
yevgeniychaplygin3 <59663599+yevgeniychaplygin3@users.noreply.github.com>
2021-11-24 17:46:34 -0800

    Update enc_server.c

--- enc_server.c
+++ enc_server.c
@@ -6,6 +6,7 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <sys/wait.h> 
+#define BUFFER_SIZE 512
 
 void error(const char *msg) { 
   perror(msg); 
@@ -17,20 +18,71 @@ void setupAddressStruct(struct sockaddr_in *address, int portNumber){
 
     address->sin_port = htons(portNumber);
 
-    address->sin_addr.s_addr = INADDR_ANY;
+    address->sin_addr.s_addr = INADDR_ANY; // set as localhost target IP address instead??
 }
 
 char* encryptData(char* string){
-    char* encryptedString = NULL;
+    // CHANGE THE BUFFER SIZE TO MAKE SURE IT FITS THE ORIGINAL STRING
+    char encryptedString[BUFFER_SIZE];
+    // get the key from the string (it is in between the special characters @@)
+    char* key = strstr(string, "@@") +2;
+    key[strcspn(key, "@@")] = '\0'; // remove special marker
+    
+    // remove special markers from the original string
+    string[strcspn(string, "@@")] = '\0';
+    // start encrypting
 
+    int charString, charKey;
 
-    return encryptedString;
+    /* This for loop will first change the ASCII values of each character
+    to a scale of 0-26. Where 0=A, 1=B and so on.. 26 is the space character.
+    this is done so we can mod 27 when we add the plaintext and the key.*/
+    for(int i=0; i<strlen(string); i++){
+        // convert to int
+        charString = string[i];
+        charKey = key[i];
+
+        // check for spaces. If character is a space, then place as last index(26)
+        // will subtract all other characters by 65 to convert to 0 to 26 scale
+        if(charKey == 32 || charString == 32){
+            if(charKey == 32){
+                charKey = 26;
+                charString -= 65;
+            }
+            if(charString == 32){
+                charString = 26;
+                charKey -= 65;
+            }
+            if(charKey == 32 && charString == 32){
+                charString = 26;
+                charKey = 26;
+            }
+        }else{ // neither character is a space (32)
+            charKey -= 65;
+            charString -= 65;
+        }
+
+        // add and mod 27 to recieve the new encrypted character
+        int newChar = (charString + charKey) % 27; 
+        // if its the last index on our scale, then it is the space character
+        if(newChar == 26){
+            newChar = 32;
+        }else{ // otherwise add 65 to convert back to ASCII
+            newChar += 65;
+        }
+        // place the newChar into the encrypted string
+        encryptedString[i] = (char) newChar;
+        
+    }
+    // overwrite the plaintext string and return. 
+    strcpy(string, encryptedString);
+    return string;
 }
 
 int main(int argc, char* argv[]){
 
     int listenSocket, portNumber, charsRead, connectionSocket;
-    char buffer[256];
+    char buffer[BUFFER_SIZE];
     struct sockaddr_in serverAddress, clientAddress;
     socklen_t sizeOfClientInfo = sizeof(clientAddress);
 
@@ -62,6 +114,20 @@ int main(int argc, char* argv[]){
         if (connectionSocket < 0){
             error("ERROR on accept");
         }
+        // check to make sure its the enc_client.
+        //if(we recive a 1 than its the client)
+        char checkClient[] = "0";
+        int check = recv(connectionSocket, checkClient, 1,0); 
+        if(strcmp(checkClient, "1") != 0 || check < 0){
+            fprintf(stderr, "Could not connect to enc_server on port %d. Disconnecting now..\n", portNumber);
+            close(listenSocket);
+            close(connectionSocket);
+
+            exit(2);
+        } 
+        //else(error) 
+
+
         printf("SERVER: Connected to client running at host %d port %d\n", ntohs(clientAddress.sin_addr.s_addr), ntohs(clientAddress.sin_port));
 
         // fork
@@ -75,36 +141,36 @@ int main(int argc, char* argv[]){
                     break;
                 // child fork
                 case 0:
-                    // check to see if its communicating with enc_client
                     // revc client data(plaintext & key)
-
-
                     // Get the message from the client and display it
-                    memset(buffer, '\0', 256);
+                    memset(buffer, '\0', BUFFER_SIZE);
                     // Read the client's message from the socket
-                    charsRead = recv(connectionSocket, buffer, 255, 0); 
+                    charsRead = recv(connectionSocket, buffer, BUFFER_SIZE, 0); 
                     if (charsRead < 0){
                         error("ERROR reading from socket");
                     }
                     printf("SERVER: I received this from the client: \"%s\"\n", buffer);
                     // ----------------------------------------------------------
                     // do encryption
-                    encryptData(buffer);
+                    char *encryptedString = encryptData(buffer);
+                    // printf("%s\n", encryptedString);
                     // ----------------------------------------------------------
                     
                     // write back ciphertext to enc_client
                     // Send a Success message back to the client
-                    charsRead = send(connectionSocket, "I am the server, and I got your message", 39, 0); 
+                    charsRead = send(connectionSocket, encryptedString, strlen(encryptedString), 0); 
                     if (charsRead < 0){
                     error("ERROR writing to socket");
                     }
                     // Close the connection socket for this client
                     close(connectionSocket);
+                    exit(0); // break out of the while loop??
                     break;
                 // parent fork
                 default:
                 waitpid(spawnpid, &childExitMethod, 0);
-                printf("Child exit status: %d", childExitMethod);                
+                printf("Child exit status: %d", childExitMethod);  
+                fflush(stdout);              
             }
     }
 
