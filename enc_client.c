df1a26ba09113fa0aa3d636f4a0f79e5fef4157c
yevgeniychaplygin3 <59663599+yevgeniychaplygin3@users.noreply.github.com>
2021-11-26 10:49:24 -0800

    Update enc_client.c

--- enc_client.c
+++ enc_client.c
@@ -4,7 +4,7 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <netdb.h> 
+#include <netdb.h> // for gethostbyname
 
 // Error function used for reporting issues
 void error(const char *msg) { 
@@ -53,6 +53,7 @@ int main(int argc, char* argv[]){
     // compare the stringBuffer to the length of keygen
     fseek(fp, 0, SEEK_END);
     fseek(fp2, 0, SEEK_END);
+    
 
     long int textFileSize = ftell(fp);
     long int keyFileSize = ftell(fp2);
@@ -61,18 +62,28 @@ int main(int argc, char* argv[]){
         fprintf(stderr, "Error: key '%s' is too short\n", argv[2]);
         exit(1);
     }
+    // set fp back to beginning of file.
+    // fseek(fp, 0, SEEK_SET);
+    // fseek(fp2, 0, SEEK_SET);
+    rewind(fp);
+    rewind(fp2);
 
+    // needed to get full string.
+    textFileSize++;
+    keyFileSize++;
     // create char array to hold the plaintext and key files
     char stringBuffer[textFileSize];
     char keyBuffer[textFileSize];
 
     // clear out buffers
-    memset(stringBuffer, '\0', sizeof(stringBuffer));
-    memset(keyBuffer, '\0', sizeof(keyBuffer));
+    memset(stringBuffer, '\0',textFileSize);
+    memset(keyBuffer, '\0', textFileSize);
 
-    // Get input from the user, trunc to buffer - 1 chars, leaving \0
-    fgets(stringBuffer, sizeof(stringBuffer)-1, fp);
-    fgets(keyBuffer, sizeof(keyBuffer)-1, fp2);
+    // Get input from the user
+    fgets(stringBuffer, textFileSize, fp);
+    fgets(keyBuffer, textFileSize, fp2);
+    // printf("stringBuffer %s\n", stringBuffer);
+    // printf("keyBuffer %s\n", keyBuffer);
     
     // check for any bad characters. (characters not in ASCII 60 to 90, space 32, 
     // nor the newline 10)
@@ -87,13 +98,13 @@ int main(int argc, char* argv[]){
         }
     }
     
-    // Add special marker to know its the end of the input
+    // Add special marker to know its the end of the plaintext input
     // Remove the trailing \n that fgets adds 
     int newLineIndex = strcspn(stringBuffer, "\n");
-    stringBuffer[newLineIndex] = '@'; 
-    stringBuffer[newLineIndex+1] = '@'; 
+    stringBuffer[newLineIndex] = '!'; 
+    stringBuffer[newLineIndex+1] = '!'; 
     stringBuffer[newLineIndex+2] = '\0'; // might not need
-
+    // add special marker to know its the end of the key
     newLineIndex = strcspn(keyBuffer, "\n");
     keyBuffer[newLineIndex] = '@'; 
     keyBuffer[newLineIndex+1] = '@'; 
@@ -120,15 +131,28 @@ int main(int argc, char* argv[]){
 
     // check to make sure its the enc_server
     // send a 1 to tell them its the client
+    char checkBuf[2];
+    memset(checkBuf, '\0', 2);
     charsWritten = send(socketFD, "1", 1, 0);
     if(charsWritten < 1){
         fprintf(stderr, "Error writing to socket\n");
         exit(2);
+    }else{
+        // recieve a message back from the client to know we are connected
+        charsWritten = recv(socketFD,checkBuf,2,0);
+        // printf("Client recieved this %s\n", checkBuf);
+        if(strncmp(checkBuf, "OK", 2) != 0 || charsWritten < 0){
+            printf("Client did not recieve OK\n");
+            exit(0);
+        }// else{printf("Client is now connected\n");}
     }
+    
+
     // Send message to server
     // Write to the server
     do{
         // send the plaintext file
+        printf("Client now sending string %s\n", stringBuffer);
         charsWritten = send(socketFD, stringBuffer, strlen(stringBuffer), 0); 
         if (charsWritten < 0){
             error("CLIENT: ERROR writing to socket");
@@ -136,7 +160,13 @@ int main(int argc, char* argv[]){
         if (charsWritten < strlen(stringBuffer)){
             printf("CLIENT: WARNING: Not all data written to socket!\n");
         }
-        // send the key
+        printf("charsWritten: %d. strlen(stringBuffer): %d\n", charsWritten, (int)strlen(stringBuffer));
+
+    }while(charsWritten < strlen(stringBuffer));
+
+    // send the key. 
+    do{
+        printf("Client now sending key %s\n", keyBuffer);
         charsWritten = send(socketFD, keyBuffer, strlen(keyBuffer), 0); 
         if (charsWritten < 0){
             error("CLIENT: ERROR writing to socket");
@@ -144,6 +174,9 @@ int main(int argc, char* argv[]){
         if (charsWritten < strlen(keyBuffer)){
             printf("CLIENT: WARNING: Not all data written to socket!\n");
         }
+
+        printf("charsWritten: %d. strlen(keyBuffer): %d\n", charsWritten, (int)strlen(keyBuffer));
+    }while(charsWritten < strlen(keyBuffer));
         
         // Clear out the buffer again for reuse
         memset(stringBuffer, '\0', sizeof(stringBuffer));
@@ -154,10 +187,16 @@ int main(int argc, char* argv[]){
         if (charsRead < 0){
             error("CLIENT: ERROR reading from socket");
         }
-    }while(charsRead < strlen(stringBuffer));   // check to see if the length of charsRead matches the length of the original string from the txt file
+        printf("charsRead: %d. stringBuffer: %s\n", charsRead, stringBuffer);
+    //    (charsRead < strlen(stringBuffer));   // check to see if the length of charsRead matches the length of the original string from the txt file
     // printf("CLIENT: I received this from the server: \"%s\"\n", stringBuffer);
+
+    
+    // use write instead and add a new line
+    // fputs(stringBuffer, stdout);
     // send to stdout here
-    fputs(stringBuffer, stdout);
+    write(1, stringBuffer, strlen(stringBuffer));
+    write(1, "\n", 1);
     // Close the socket
     close(socketFD); 
     // Close the files
