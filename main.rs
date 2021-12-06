5e46714514ccfe7599e5752a8016e1d062be7e48
yevgeniychaplygin3 <59663599+yevgeniychaplygin3@users.noreply.github.com>
2021-12-06 12:17:00 -0800

    Update main.rs

--- main.rs
+++ main.rs
@@ -149,22 +149,31 @@ fn main() {
 
     // CHANGE CODE: Add code that does the following:
     // 1. Calls partition_data to partition the data into equal partitions
-    let mut vOfv = partition_data(num_partitions, &intermediate_sums);
+    let v_of_v =partition_data(num_partitions, &v); 
     // 2. Calls print_partition_info to print info on the partitions that have been created
-    print_partition_info(&vOfv);
+    print_partition_info(&v_of_v);
     // 3. Creates one thread per partition and uses each thread to concurrently process one partition
-    
-    for i in 1..num_partitions{
-        let partition = vOfv[i].clone();
+    // let res;
+    // for i in 1..num_partitions{
+    //     let partition = vOfv[i].clone();
 
-        let handle = thread::spawn(move || map_data(&partition));
-        let res = handle.join().unwrap(); 
+    //     let handle = thread::spawn(move || map_data(&partition));
+    //     res = handle.join().unwrap(); 
+
         
-    }
+        
+    // }
+    
     // 4. Collects the intermediate sums from all the threads
     // 5. Prints information about the intermediate sums
+    // use another for loop and create new vectors for each
+    // println!("Intermediate sums = {:?}", intermediate_sums);
+    // intermediate_sums.push(res);
     // 5. Calls reduce_data to process the intermediate sums
+
+    // let sum = reduce_data(v: &Vec<usize>);
     // 6. Prints the final sum computed by reduce_data
+    // println!("Sums = { }", sum);
 
 }
 
@@ -183,6 +192,32 @@ fn main() {
 * 
 */
 fn partition_data(num_partitions: usize, v: &Vec<usize>) -> Vec<Vec<usize>>{
-    // Remove the following line which has been added to remove a compiler error
-    partition_data_in_two(v)
+    let mut xs: Vec<Vec<usize>> = Vec::new();
+
+    let partition_size = v.len() / num_partitions;
+    let mut count = 0;
+    let mut remandier = v.len()%num_partitions;
+
+    for _i in 0..num_partitions{
+        // create a new vector
+        let mut x1 : Vec<usize> = Vec::new();
+        let mut end_range = count + partition_size;
+
+        if remandier > 0 {
+            end_range += 1;
+        }
+        
+        for j in count..end_range{
+            // add all j elements to vector x1
+            x1.push(v[j]);
+        }
+        // push vector x1 on to vector of vectors xs
+        xs.push(x1);
+        if remandier > 0{
+            remandier -= 1;
+        }
+        count += partition_size;
+    }
+    println!("xs: {:?}", xs);
+    xs
 }
